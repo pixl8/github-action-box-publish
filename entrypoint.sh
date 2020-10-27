515b2df9530d087b5d11a05442d7bf6296d5b8c0
Dominic Watson <watson.dominic@gmail.com>
2020-10-27 23:44:41 +0000

    Use input variables correctly and try another way to do variable replacement in box.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,17 @@ FULL_DIR="${GITHUB_WORKSPACE}${BOXJSON_DIR}"
 BOX_JSON_FILE="${FULL_DIR}/box.json"
 DO_ENV_SUBSTITUTION="true"
 
-if [[ -z "$forgebox_user" ]] || [[ -z "$forgebox_pass" ]] ; then
-	echo "Forgebox environment variables not set. Please set both forgebox_user and forgebox_pass environment variables to use this action."
+if [[ -z "$INPUT_FORGEBOX_USER" ]] || [[ -z "$INPUT_FORGEBOX_PASS" ]] ; then
+	echo "Forgebox environment variables not set. Please set both FORGEBOX_USER and FORGEBOX_PASS environment variables to use this action."
 	exit 1
 fi
 
 if [[ -f $BOX_JSON_FILE ]] ; then
-	if [[ "$DO_ENV_SUBSTITUTION" == "true" ]] ; then
-		envsubst < $BOX_JSON_FILE > $BOX_JSON_FILE.substituted
-		rm $BOX_JSON_FILE
-		mv $BOX_JSON_FILE.substituted $BOX_JSON_FILE
+	if [[ -n "${INPUT_VERSION_NUMBER}" ]] ; then
+		sed -i "s,{VERSION_NUMBER},$INPUT_VERSION_NUMBER," $BOX_JSON_FILE
+	fi
+	if [[ -n "${INPUT_DOWNLOAD_LOCATION}" ]] ; then
+		sed -i "s,{DOWNLOAD_LOCATION},$INPUT_DOWNLOAD_LOCATION," $BOX_JSON_FILE
 	fi
 
 	echo "Publishing box.json to Forgebox:"
@@ -23,7 +24,7 @@ if [[ -f $BOX_JSON_FILE ]] ; then
 	echo "--------------------------------"
 
 
-	box forgebox login username="$forgebox_user" password="$forgebox_pass" || exit 1;
+	box forgebox login username="$INPUT_FORGEBOX_USER" password="$INPUT_FORGEBOX_PASS" || exit 1;
 	box publish directory="$FULL_DIR" || exit 1;
 
 	echo ""
