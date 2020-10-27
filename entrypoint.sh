aab14a47003fbca1381c29e437f26d46023a027f
Dominic Watson <watson.dominic@gmail.com>
2020-10-27 22:24:30 +0000

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,8 @@ if [[ -f $BOX_JSON_FILE ]] ; then
 		mv $BOX_JSON_FILE.substituted $BOX_JSON_FILE
 	fi
 
+	echo "Download url: $DOWNLOAD_URL"
+
 	box forgebox login username="$FORGEBOX_USER" password="$FORGEBOX_PASS" || exit 1;
 	box publish directory="$FULL_DIR" || exit 1;
 
